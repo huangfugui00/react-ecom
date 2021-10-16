58fd1792b3341512caed0299e4f913a98d02d6bf
hfg <15011907262@163.com>
2021-10-16 22:33:27 +0800

    完成BlogDisplay

--- src/pages/home/BlogDisplay.js
+++ src/pages/home/BlogDisplay.js
@@ -3,26 +3,28 @@ import './BlogDisplay.scss'
 
 const BlogDiv = ({blog}) =>{
     return(
-        <div className="blog-div">
-            <img src={blog.img}/>
+        <div  className="blog-div">
+            <img src={blog.img} className="w-100"/>
             <p>By {blog.author} on {blog.createdAt}</p>
             <h3>{blog.title}</h3>
-            <div>
+            <div className="" id="blog-content">
                 {blog.content}
             </div>
         </div>
+   
+       
     )
 }
 
 const BlogDisplay = ({blogs}) => {
     return (
-        <div>
+        <div className="main">
             <h1>Our Blogs</h1>
             <div className="blogs-div row">          
                 {                
                     blogs.slice(0,3).map((blog,index)=> 
-                        <div className="col-lg-4">
-                            <BlogDiv blog={blog}/>
+                        <div className="col-xl-4 col-md-6" >
+                                <BlogDiv blog={blog}/>
                         </div>
                     )
                 }     
